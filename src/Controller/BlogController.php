8094323355e286f5da88bdc29a35090843c5d8cc
Robin <gressier.robin@gmail.com>
2022-12-18 20:26:43 +0100

    fixture ajouté + template show

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,21 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    #[Route('/blog', name: 'blog')]
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
+        $repo = $entityManager->getRepository(Article::class);
+
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
     #[Route('/', name: 'home')]
     public function home() {
         return $this->render('blog/home.html.twig');
+    }
+
+    #[Route('/blog/12', name:'blog_show')]
+    public function show() {
+        return $this->render('blog/show.html.twig');
     }
 }
